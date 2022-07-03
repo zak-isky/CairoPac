642ac5f08873b85983b42a373d3b3dbfaaa3fb22
mabdelzaher <mabdelzaher@iskydev.com>
2022-07-03 10:54:58 +0200

    Add[report xls]

---
+++ report_xls/__manifest__.py
@@ -0,0 +1,26 @@
+
+{
+    'name': 'Report in Excel',
+    'version': '15.0.1.0.0',
+    'summary': "Report in Excel",
+    'description': "",
+    'category': '',
+    'author': '',
+    'maintainer': '',
+    'company': '',
+    'website': '',
+    'depends': [
+                'base'
+                ],
+    'data': [
+            ],
+    'assets': {
+        'web.assets_backend': [
+            'report_xls/static/src/js/action_manager.js',
+        ],
+    },
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'auto_install': False,
+}
